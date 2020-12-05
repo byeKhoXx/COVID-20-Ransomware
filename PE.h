fc3ddedad13f7e9f63a06a3d9b1e1f02ee291969
byeKhoXx <douha.ismael@gmail.com>
2020-12-05 18:35:46 +0100

    InfectPE working + shellcode working

--- PE.h
+++ PE.h
@@ -1,27 +1,26 @@
-#pragma once
 #include <vector>
-#define __STDC_WANT_LIB_EXT1__ 1
-#include <string.h>
+#pragma once
 
+using namespace std;
 namespace PE
 {
-	struct PE_FILE
-	{
-		size_t size_ids{};
-		size_t size_dos_stub{};
-		size_t size_inh32{};
-		size_t size_ish{};
-		size_t size_sections{};
-		IMAGE_DOS_HEADER ids;
-		std::vector<char> MS_DOS_STUB;
-		IMAGE_NT_HEADERS32 inh32;
-		std::vector<IMAGE_SECTION_HEADER> ish;
-		std::vector<std::shared_ptr<char>> Sections;
-		void set_sizes(size_t, size_t, size_t, size_t, size_t);
-	};
+    struct PE_FILE
+    {
+        size_t size_ids{};
+        size_t size_dos_stub{};
+        size_t size_inh32{};
+        size_t size_ish{};
+        size_t size_sections{};
+        IMAGE_DOS_HEADER ids;
+        vector<char> MS_DOS_STUB;
+        IMAGE_NT_HEADERS32 inh32;
+        vector<IMAGE_SECTION_HEADER> ish;
+        vector<std::shared_ptr<char>> Sections;
+        void set_sizes(size_t, size_t, size_t, size_t, size_t);
+    };
+    std::tuple<bool, char *, std::streampos> OpenBinary(std::string filename);
+    PE_FILE ParsePE(const char *PE);
+    void WriteBinary(PE_FILE pefile, std::string file_name, size_t size);
+    void Inject_Resize_Code(char* pe_file, size_t size_of_pe, char xcode[], size_t size_of_xcode, const std::string& out_path);
 
-	std::tuple<bool, char*, std::streampos> OpenBinary(std::string filename);
-	PE_FILE ParsePE(const char* PE);
-	void WriteBinary(PE_FILE pefile, std::string file_name, size_t size);
-	void Inject_Resize_Code(char* pe_file, size_t size_of_pe, char xcode[], size_t size_of_xcode, const std::string& out_path);
 }
